c2a00ead3944b8771adea4f88f6fc125dbd1cd08
Maros Kollar <maros@k-1.com>
2017-06-02 23:02:00 +0200

    Release 1.19

--- lib/DateTime/Format/CLDR.pm
+++ lib/DateTime/Format/CLDR.pm
@@ -18,7 +18,7 @@ our @EXPORT_OK = qw( cldr_format cldr_parse );
 
 # CPAN data
 our $AUTHORITY = 'cpan:MAROS';
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 # Default format if none is set
 our $DEFAULT_FORMAT = 'date_format_medium';
